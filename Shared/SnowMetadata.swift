79ca5aa1c26a10ea8bfb3207029d338503b510cc
Snowy <3mille.prenom.nom@gmail.com>
2021-02-10 19:50:55 +0100

    Add SnowMetadata

---
+++ Shared/SnowMetadata.swift
@@ -0,0 +1,29 @@
+//
+//  SnowMetadata.swift
+//  Snowymage
+//
+//  Created by Emil Pedersen on 10/02/2021.
+//
+
+import Foundation
+
+struct SnowMetadata: OptionSet {
+    var rawValue: UInt8
+    
+    static let alpha = SnowMetadata(rawValue: 1 << 0)
+    static let grayscale = SnowMetadata(rawValue: 1 << 1)
+    static let palette = SnowMetadata(rawValue: 1 << 3)
+    static let paletteCompression = SnowMetadata(rawValue: 1 << 2)
+    static let clip = SnowMetadata(rawValue: 1 << 4)
+    static let small = SnowMetadata(rawValue: 1 << 5)
+    
+    var bytesPerPixel: Int {
+        (self.contains(.grayscale) ? 1 : 3) + (self.contains(.alpha) ? 1 : 0)
+    }
+}
+
+extension SnowMetadata: CustomStringConvertible {
+    var description: String {
+        "SnowMetadata \(rawValue) [alpha=\(self.contains(.alpha)), grayscaled=\(self.contains(.grayscale)), palette=\(self.contains(.palette) ? self.contains(.paletteCompression) ? "compressed" : "uncompressed" : "none"), grayscaled=\(self.contains(.grayscale))]"
+    }
+}
