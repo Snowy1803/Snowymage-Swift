0b67ee25fa77bbd84b358900bf4bac8ff1d003e7
Snowy <3mille.prenom.nom@gmail.com>
2021-02-11 18:14:22 +0100

    Thumbnails keep aspect ratio

--- Thumbnail Mac/ThumbnailProvider.swift
+++ Thumbnail Mac/ThumbnailProvider.swift
@@ -10,19 +10,28 @@ import QuickLookThumbnailing
 class ThumbnailProvider: QLThumbnailProvider {
     
     override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
-        handler(QLThumbnailReply(contextSize: request.maximumSize, drawing: { (context) -> Bool in
+        
+        guard let data = try? Data(contentsOf: request.fileURL) else {
+            handler(nil, CocoaError(.fileReadCorruptFile))
+            return
+        }
+        
+        var reader = SnowReader(source: data)
+        
+        guard let img = reader.read() else {
+            handler(nil, CocoaError(.fileReadCorruptFile))
+            return
+        }
+        
+        let width = CGFloat(img.width)
+        let height = CGFloat(img.height)
+        
+        let scaleX = request.maximumSize.width / width
+        let scaleY = request.maximumSize.height / height
+        let scale = min(scaleX, scaleY)
+        
+        handler(QLThumbnailReply(contextSize: CGSize(width: scale * width, height: scale * height), drawing: { (context) -> Bool in
             // Draw the thumbnail here.
-            
-            guard let data = try? Data(contentsOf: request.fileURL) else {
-                return false
-            }
-            
-            var reader = SnowReader(source: data)
-            
-            guard let img = reader.read() else {
-                return false
-            }
-            
             context.draw(img, in: CGRect(x: 0, y: 0, width: context.width, height: context.height))
             // Return true if the thumbnail was successfully drawn inside this block.
             return true
